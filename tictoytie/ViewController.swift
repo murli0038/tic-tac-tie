c3d3790b08841456afe902a275fa4e8f2bef6a4d
COE-029 <coe-029@coe-029s-imac.local>
2019-09-06 13:52:35 +0530

    Initial Commit

---
+++ tictoytie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tictoytie
+//
+//  Created by COE-029 on 06/09/19.
+//  Copyright © 2019 COE-029. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
